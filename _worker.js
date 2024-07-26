b3a8dd7b9c447ccfbe88ed4da8064bb95091c9ee
EVEOD <97860920+EVEOD@users.noreply.github.com>
2024-07-26 17:08:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'aed208b6-1079-4fd2-be74-d101cc8295fa';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
